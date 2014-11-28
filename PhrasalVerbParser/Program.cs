ea7bbf43b872b823d23ae513c478a25cc86421dc
Alexandre Point <apoint@fleex.tv>
2014-11-28 18:43:40 +0100

    Compare fleex and usingenglish.com phrasal verbs

--- PhrasalVerbParser/Program.cs
+++ PhrasalVerbParser/Program.cs
@@ -16,24 +16,50 @@ namespace PhrasalVerbParser
         
         static void Main(string[] args)
         {
+            var phrasalVerbFilePath = PathToApplication + "Resources/phrasalVerbs";
+
+            /*// parsing of usingenglish phrasal verbs
             var parser = new UsingEnglishParser();
             var phrasalVerbs = parser.ParseAllPhrasalVerbs();
-
             Console.WriteLine("============");
             Console.WriteLine("Found {0} phrasal verbs", phrasalVerbs.Count);
 
+            // persisting list of phrasal verbs
             Console.WriteLine("============");
-            var phrasalVerbFilePath = PathToApplication + "Resources/phrasalVerbs";
             Console.WriteLine("Persisting phrasal verbs to {0}", phrasalVerbFilePath);
             PersistPhrasalVerbs(phrasalVerbs, phrasalVerbFilePath);
-            Console.WriteLine("Persisted phrasal verbs");
+            Console.WriteLine("Persisted phrasal verbs");*/
 
             Console.WriteLine("============");
             Console.WriteLine("Reading phrasal verbs from file");
             var verbs = ReadPhrasalVerbs(phrasalVerbFilePath);
             Console.WriteLine("Retrieved {0} verbs", verbs.Count);
 
+            var fleexPhrasalVerbs = ReadFleexPhrasalVerbs();
+
+            var fleexOnlyPv = fleexPhrasalVerbs.Except(verbs.Select(v => v.Name)).ToList();
+            Console.WriteLine("{0} phrasal verbs missing on using english:", fleexOnlyPv.Count);
+            foreach (var pv in fleexOnlyPv)
+            {
+                Console.WriteLine(pv);
+            }
+
+            Console.WriteLine("-------------------------");
+            var missingPvOnFleex = verbs.Select(v => v.Name).Except(fleexPhrasalVerbs);
+            Console.WriteLine("{0} phrasal verbs missing on fleex", missingPvOnFleex.Count());
+            foreach (var pv in missingPvOnFleex)
+            {
+                Console.WriteLine(pv);
+            }
+
             Console.ReadKey();
+        }
+
+        private static List<string> ReadFleexPhrasalVerbs()
+        {
+            var pathToFile = PathToApplication + "Resources/fleexPhrasalVerbs.txt";
+            var allLines = File.ReadAllLines(pathToFile).ToList();
+            return allLines;
         }
 
         
