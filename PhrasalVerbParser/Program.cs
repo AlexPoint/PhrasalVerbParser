5a16ed0ffe6da43db629af46d0684759c45b1cdb
Alexandre Point <apoint@fleex.tv>
2015-01-09 18:57:33 +0100

    New: updated OpenNlp package from 1.2.0 -> 1.2.3

--- PhrasalVerbParser/Program.cs
+++ PhrasalVerbParser/Program.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Web.UI.WebControls.WebParts;
 using System.Xml.Serialization;
 using LemmaSharp.Classes;
+using OpenNLP.Tools.Parser;
 using OpenNLP.Tools.Trees;
 using PhrasalVerbParser.src;
 
@@ -17,75 +18,85 @@ namespace PhrasalVerbParser
     class Program
     {
         private static readonly string PathToApplication = Directory.GetCurrentDirectory() + "/../../";
+
         
         static void Main(string[] args)
         {
-            var sentence = "You should Get up and give the elderly man your seat.";
-            var modelPath = PathToApplication + "Resources/OpenNlp/Models/";
-            var parser = new OpenNLP.Tools.Parser.EnglishTreebankParser(modelPath, true, false);
-            var parse = parser.DoParse(sentence);
-            // Extract dependencies from lexical tree
-            var tlp = new PennTreebankLanguagePack();
-            var gsf = tlp.GrammaticalStructureFactory();
-            var tree = new ParseTree(parse);
-            Console.WriteLine(tree);
-            var gs = gsf.NewGrammaticalStructure(tree);
-            var dependencies = gs.TypedDependencies();
-
-            foreach (var dep in dependencies)
-            {
-                Console.WriteLine(dep);
-            }
-
-
-            var phrasalVerbFilePath = PathToApplication + "Resources/phrasalVerbs";
-
+            // Lemmatizer
             var fullPathToSrcData = PathToApplication + "Resources/lemmatizer/en_lemmatizer_data.lem";
             var stream = File.OpenRead(fullPathToSrcData);
             var lemmatizer = new Lemmatizer(stream);
 
-            /*var parser = new UsingEnglishParser();
-            var phrasalVerbs = parser.ParseAllPhrasalVerbs();
-            Console.WriteLine("============");
-            Console.WriteLine("Found {0} phrasal verbs", phrasalVerbs.Count);*/
+            var sentences = new List<string>()
+            {
+                "I could really do with a cup of tea.",
+                "The United Kingdom did away with the death penalty in 1965.",
+                "He ducked out of helping us last night.",
+                "The government is homing in on benefit fraud.",
+                "The company honed in on its rival and tried to take it over.",
+                "Many learners shy away from using phrasal verbs.",
+                "He's trying to steer clear of his lecturer because he hasn't finished his assignment yet.",
+                "You can't just walk away from your problems.",
+                "He always manages to wriggle out of any extra work we get.",
+                "The police have zeroed in on the man they believe to be responsible for the murder.",
+                "The hurricane is zeroing in on Florida."
+            };
+            foreach (var sentence in sentences)
+            {
+                Console.WriteLine(sentence);
+                var deps = ComputeDependencies(sentence);
+                foreach (var typedDependency in deps)
+                {
+                    Console.WriteLine(typedDependency + " (" + lemmatizer.Lemmatize(typedDependency.Gov().GetWord()) + ", " + lemmatizer.Lemmatize(typedDependency.Dep().GetWord()) + ")");
+                } 
+                Console.WriteLine("=============");
+            }
 
-
+            /*
+            // load phrasal verbs & examples
+            var phrasalVerbFilePath = PathToApplication + "Resources/phrasalVerbs";
             var phrasalVerbs = ReadPhrasalVerbs(phrasalVerbFilePath);
 
-            // parse dependencies
-            /*var tuples = new List<Tuple<string, string, List<DependencyRelationship>>>();
-            var lines = new List<string>();
+            // 
+            var results = new List<Tuple<string, string, List<TypedDependency>>>();
             foreach (var phrasalVerb in phrasalVerbs)
             {
                 foreach (var usage in phrasalVerb.Usages)
                 {
-                    var cleanupExample = LowerCaseAllUpperCasedWords(usage.Example);
-                    var dependencies = DependencyParser.ParseDepencyRelationshipsInSentence(cleanupExample);
-                    
-                    Console.WriteLine(cleanupExample);
-                    //var relationships = dependencies.Where(d => d.RelationshipType == "prt");
+                    // compute dependencies
+                    var example = LowerCaseAllUpperCasedWords(usage.Example);
+                    var dependencies = ComputeDependencies(example);
+
+                    Console.WriteLine("{0} -> {1}", phrasalVerb.Name, example);
+                    // get relevant dependencies found
                     var parts = phrasalVerb.Name.Split(' ');
-                    if (parts.Count() > 2)
-                    {
-                        Console.WriteLine("{0} parts for phrasal verb {1}", parts.Count(), string.Join("|", parts));
-                    }
                     var root = parts.First();
                     var last = parts.Last();
-                    var relevantRelationships =
-                        dependencies.Where(d => (root == lemmatizer.Lemmatize(d.Dep.Word) && last == d.Gov.Word)
-                        || (root == lemmatizer.Lemmatize(d.Gov.Word) && last == d.Dep.Word));
-                    var secondRelevantRelationships =
-                        dependencies.Where(d => last == d.Gov.Word || last == d.Dep.Word);
-                    Console.WriteLine(string.Join("|", relevantRelationships));
-                    var line = string.Format("{0}|{1}/{2}|{3}|{4}", cleanupExample, root, last,
-                        string.Join("||", relevantRelationships), string.Join("||", secondRelevantRelationships));
-                    lines.Add(line);
-
-                    tuples.Add(new Tuple<string, string, List<DependencyRelationship>>(phrasalVerb.Name, cleanupExample, relevantRelationships.ToList()));
-                    Console.WriteLine("---------");
+                    var relevantRelationships = dependencies
+                        .Where(d => (root == lemmatizer.Lemmatize(d.Dep().GetWord()) && last == d.Gov().GetWord())
+                                    || (root == lemmatizer.Lemmatize(d.Gov().GetWord()) && last == d.Dep().GetWord()))
+                                    .ToList();
+                    results.Add(new Tuple<string, string, List<TypedDependency>>(phrasalVerb.Name, example, relevantRelationships));
                 }
+            }
+            Console.WriteLine("===========================");
+
+            // Print results
+            var nbOfFaselyDetectedPhrasalVerb = results.Count(tup => tup.Item3.Count != 1);
+            Console.WriteLine("{0} (/ {1}) phrasal verbs fasely detected", nbOfFaselyDetectedPhrasalVerb, results.Count);
+            Console.WriteLine("----------");
+            foreach (var group in results.Where(tup => tup.Item3.Count != 1).GroupBy(tup => tup.Item3.Count))
+            {
+                Console.WriteLine(group.Key + ":");
+                foreach (var tuple in group)
+                {
+                    Console.WriteLine("{0} - {1}", tuple.Item1, tuple.Item2);
+                }
+                Console.WriteLine("--------------");
             }*/
 
+            
+            
             /*var outputFilePath = PathToApplication + "Resources/output.txt";
             File.WriteAllLines(outputFilePath, lines);
 
@@ -105,18 +116,18 @@ namespace PhrasalVerbParser
             Console.WriteLine("Persisted phrasal verbs");*/
 
             // persisting examples
-            var pvExamplesFilePath = PathToApplication + "Resources/phrasalVerbsExamples.txt";
+            /*var pvExamplesFilePath = PathToApplication + "Resources/phrasalVerbsExamples.txt";
             PersistPhrasalVerbsAndExamples(phrasalVerbs, pvExamplesFilePath);
             Console.WriteLine("Persisted examples");
-            Console.WriteLine("-------------------------");
+            Console.WriteLine("-------------------------");*/
             
             // stats on usingenglish phrasal verbs
-            var verbs = ReadPhrasalVerbs(phrasalVerbFilePath);
+            /*var verbs = ReadPhrasalVerbs(phrasalVerbFilePath);
             var nbOfSeparableVerbs = verbs.Count(v => v.Usages.All(u => u.SeparableMandatory));
             var nbOfInseparableVerbs = verbs.Count(v => v.Usages.All(u => u.Inseparable));
             Console.WriteLine("{0} separable verbs", nbOfSeparableVerbs);
             Console.WriteLine("{0} inseparable verbs", nbOfInseparableVerbs);
-            Console.WriteLine("{0} verbs", verbs.Count);
+            Console.WriteLine("{0} verbs", verbs.Count);*/
 
             // write a file with examples of phrasal verbs
             /*var pathToOutputFile = PathToApplication + "Resources/phrasalVerbExamples.txt";
@@ -133,11 +144,46 @@ namespace PhrasalVerbParser
             Console.ReadKey();
         }
 
+        private static EnglishTreebankParser _parser;
+        private static EnglishTreebankParser GetParser()
+        {
+            if (_parser == null)
+            {
+                var modelPath = PathToApplication + "Resources/OpenNlp/Models/";
+                _parser = new EnglishTreebankParser(modelPath, true, false);
+            }
+            return _parser;
+        }
+
+        private static IEnumerable<TypedDependency> ComputeDependencies(string sentence)
+        {
+            var parser = GetParser();
+            var parse = parser.DoParse(sentence);
+            // Extract dependencies from lexical tree
+            var tlp = new PennTreebankLanguagePack();
+            var gsf = tlp.GrammaticalStructureFactory();
+            var tree = new ParseTree(parse);
+            //Console.WriteLine(tree);
+            try
+            {
+                var gs = gsf.NewGrammaticalStructure(tree);
+                return gs.TypedDependencies();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Exception when computing deps for {0}", sentence);
+                return new List<TypedDependency>();
+            }
+        }
+
         private static string LowerCaseAllUpperCasedWords(string sentence)
         {
             var replacedSentence = Regex.Replace(sentence, "[A-Z]{2,}", s => s.Value.ToLower());
             return replacedSentence;
         }
+
+
+        // Reading / Writing phrasal verbs ----------------------------
 
         private static void PersistPhrasalVerbsAndExamples(List<PhrasalVerb> verbs, string filePath)
         {
